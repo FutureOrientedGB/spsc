bf5b5990ab0d3ecb0cfe76fdb40b7f41706d33d4
Capric <capric8416@gmail.com>
2024-07-26 15:45:34 +0800

    remove mpsc and mpmc

--- spsc.hpp
+++ spsc.hpp
@@ -7,22 +7,21 @@
 #include <algorithm>
 #include <atomic>
 #include <memory>
-#include <mutex>
 #include <vector>
 
 
 
-inline uint32_t roundup_pow_of_two(uint32_t value)
+inline uint32_t roundup_pow_of_two(uint32_t n)
 {
-	value--;
-	value |= value >> 1;
-	value |= value >> 2;
-	value |= value >> 4;
-	value |= value >> 8;
-	value |= value >> 16;
-	value++;
+	n--;
+	n |= n >> 1;
+	n |= n >> 2;
+	n |= n >> 4;
+	n |= n >> 8;
+	n |= n >> 16;
+	n++;
 
-	return value;
+	return n;
 }
 
 
@@ -89,7 +88,7 @@ public:
 		return put(input_buffer.data(), (uint32_t)input_buffer.size());
 	}
 
-	uint32_t put(const uint8_t *input_buffer, uint32_t length)
+	uint32_t put(const T *input_buffer, uint32_t length)
 	{
 		uint32_t size = (uint32_t)m_ring_buffer.size();
 
@@ -98,26 +97,25 @@ public:
 			return 0;
 		}
 
-		// ensure that we sample the output index before we start putting bytes into the buffer
+		// ensure that we sample the input offset before we start putting bytes into the buffer
 		std::atomic_thread_fence(std::memory_order_acquire);
 
 		uint32_t write_offset = load_relaxed(m_input_offset) & (size - 1);
 
 		// first put the data starting from in to buffer end
 		uint32_t first_part = std::min(length, size - write_offset);
-		std::memcpy(m_ring_buffer.data() + write_offset, input_buffer, first_part);
+		std::memcpy(m_ring_buffer.data() + write_offset, input_buffer, sizeof(T) * first_part);
 
 		// then put the rest (if any) at the beginning of the buffer
-		std::memcpy(m_ring_buffer.data(), input_buffer + first_part, length - first_part);
+		std::memcpy(m_ring_buffer.data(), input_buffer + first_part, sizeof(T) * (length - first_part));
 
-		// ensure that we add the bytes to the buffer before we update the input index
+		// ensure that we add the bytes to the buffer before we update the input offset
 		std::atomic_thread_fence(std::memory_order_release);
 
 		store_relaxed(m_input_offset, load_relaxed(m_input_offset) + length);
 
 		return length;
 	}
-
 
 	uint32_t get(std::vector<T> &output_buffer)
 	{
@@ -133,7 +131,7 @@ public:
 			return 0;
 		}
 
-		// ensure that we sample the input index before we start removing bytes from the buffer
+		// ensure that we sample the output offset before we start removing bytes from the buffer
 		std::atomic_thread_fence(std::memory_order_acquire);
 
 		uint32_t size = (uint32_t)m_ring_buffer.size();
@@ -142,12 +140,12 @@ public:
 
 		// first get the data from out until the end of the buffer
 		uint32_t first_part = std::min(length, size - read_offset);
-		std::memcpy(output_buffer, m_ring_buffer.data() + read_offset, first_part);
+		std::memcpy(output_buffer, m_ring_buffer.data() + read_offset, sizeof(T) * first_part);
 
 		// then get the rest (if any) from the beginning of the buffer
-		std::memcpy(output_buffer + first_part, m_ring_buffer.data(), length - first_part);
+		std::memcpy(output_buffer + first_part, m_ring_buffer.data(), sizeof(T) * (length - first_part));
 
-		// ensure that we remove the bytes from the buffer before we update the output index
+		// ensure that we remove the bytes from the buffer before we update the output offset
 		std::atomic_thread_fence(std::memory_order_release);
 
 		store_relaxed(m_output_offset, load_relaxed(m_output_offset) + length);
@@ -157,10 +155,13 @@ public:
 
 
 protected:
-	inline uint32_t load_relaxed(const std::atomic<uint32_t> &offset) const {
+	inline uint32_t load_relaxed(const std::atomic<uint32_t> &offset) const
+	{
 		return offset.load(std::memory_order_relaxed);
 	}
-	inline void store_relaxed(std::atomic<uint32_t> &offset, uint32_t value) {
+
+	inline void store_relaxed(std::atomic<uint32_t> &offset, uint32_t value)
+	{
 		offset.store(value, std::memory_order_relaxed);
 	}
 
@@ -169,116 +170,5 @@ private:
 	std::vector<T> m_ring_buffer;  // the buffer holding the data
 	std::atomic<uint32_t> m_input_offset;  // data is added at offset: m_input_offset % (size - 1)
 	std::atomic<uint32_t> m_output_offset;  // data is extracted from offset: m_output_offset % (size - 1)
-};
-
-
-
-// thread-safe mutiple-producers single-consumer buffer
-template<typename T>
-class mpsc
-{
-public:
-	mpsc()
-	{
-	}
-
-	mpsc(uint32_t size)
-	{
-		m_spsc.reset(size);
-	}
-
-	~mpsc()
-	{
-		m_spsc.reset(0);
-	}
-
-	bool is_null()
-	{
-		return m_spsc.is_null();
-	}
-
-	uint32_t put(const std::vector<T> &input_buffer)
-	{
-		std::lock_guard<std::mutex> lock(m_produce_lock);
-		return m_spsc.put(input_buffer);
-	}
-
-	uint32_t put(const uint8_t *input_buffer, uint32_t length)
-	{
-		std::lock_guard<std::mutex> lock(m_produce_lock);
-		return m_spsc.put(input_buffer, length);
-	}
-
-	uint32_t get(std::vector<T> &output_buffer)
-	{
-		return m_spsc.get(output_buffer);
-	}
-
-	uint32_t get(T *output_buffer, uint32_t length)
-	{
-		return m_spsc.get(output_buffer, length);
-	}
-
-
-private:
-	lock_free_spsc<T> m_spsc;
-	std::mutex m_produce_lock;
-};
-
-
-
-// thread-safe mutiple-producers mutiple-consumers buffer
-template<typename T>
-class mpmc
-{
-public:
-	mpmc()
-	{
-	}
-
-	mpmc(uint32_t size)
-	{
-		m_spsc.reset(size);
-	}
-
-	~mpmc()
-	{
-		m_spsc.reset(0);
-	}
-
-	bool is_null()
-	{
-		return m_spsc.is_null();
-	}
-
-	uint32_t put(const std::vector<T> &input_buffer)
-	{
-		std::lock_guard<std::mutex> lock(m_produce_lock);
-		return m_spsc.put(input_buffer);
-	}
-
-	uint32_t put(const uint8_t *input_buffer, uint32_t length)
-	{
-		std::lock_guard<std::mutex> lock(m_produce_lock);
-		return m_spsc.put(input_buffer, length);
-	}
-
-	uint32_t get(std::vector<T> &output_buffer)
-	{
-		std::lock_guard<std::mutex> lock(m_consume_lock);
-		return m_spsc.get(output_buffer);
-	}
-
-	uint32_t get(T *output_buffer, uint32_t length)
-	{
-		std::lock_guard<std::mutex> lock(m_consume_lock);
-		return m_spsc.get(output_buffer, length);
-	}
-
-
-private:
-	lock_free_spsc<T> m_spsc;
-	std::mutex m_produce_lock;
-	std::mutex m_consume_lock;
 };
 
